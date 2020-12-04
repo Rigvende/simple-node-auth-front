290be476f0b0186af0b9fd4acc8a31d3ce2d82cd
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-04 19:35:51 +0300

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,24 +3,25 @@ import { Link } from 'react-router-dom';
 
 export const Pagination = ({ pager }) => {
     const { length, currentPage } = pager;
+    console.log(currentPage + " " + length); 
  
     return (
         <>
             { pager && length > 0 &&
                 <ul className="pagination">
-                    <li className={`page-item first-item`} disabled={currentPage === 1 ? true : false}>
+                    <li className={`page-item first-item ${currentPage === 1 ? 'disabled' : ''}`}>
                         <Link to={`/users/${currentPage}`} className="page-link">First</Link>
                     </li>
-                    <li className={`page-item previous-item`} disabled={currentPage === 1 ? true : false}>
+                    <li className={`page-item previous-item ${currentPage === 1 ? 'disabled' : ''}`}>
                         <Link to={`/users/${currentPage - 1}`} className="page-link">&lt;&lt;</Link>
                     </li>
                     <li key={currentPage} className={`page-item number-item`}>
                         <Link to={`/users/${currentPage}`} className="page-link">{currentPage}</Link>
                     </li>
-                    <li className={`page-item next-item`} disabled={currentPage === length ? true : false}>
+                    <li className={`page-item next-item} ${currentPage === length ? 'disabled' : ''}`}>
                         <Link to={`/users/${currentPage + 1}`} className="page-link">&gt;&gt;</Link>
                     </li>
-                    <li className={`page-item last-item`} disabled={currentPage === length ? true : false}>
+                    <li className={`page-item last-item ${currentPage === length ? 'disabled' : ''}`}>
                         <Link to={`/users/${length}`} className="page-link">Last</Link>
                     </li>
                 </ul>
