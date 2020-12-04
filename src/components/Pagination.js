7528219e4915670b6596b9613fa1e83c67c54be2
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-04 19:46:39 +0300

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,26 +3,46 @@ import { Link } from 'react-router-dom';
 
 export const Pagination = ({ pager }) => {
     const { length, currentPage } = pager;
-    console.log(currentPage + " " + length); 
- 
+    console.log(currentPage + " " + length);
+
     return (
         <>
             { pager && length > 0 &&
                 <ul className="pagination">
                     <li className={`page-item first-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                        <Link to={`/users/${currentPage}`} className="page-link">First</Link>
+                        <Link
+                            to={`/users/1`}
+                            className="page-link">
+                            First
+                            </Link>
                     </li>
                     <li className={`page-item previous-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                        <Link to={`/users/${currentPage - 1}`} className="page-link">&lt;&lt;</Link>
+                        <Link
+                            to={`/users/${currentPage - 1}`}
+                            className="page-link">
+                            &lt;&lt;
+                        </Link>
                     </li>
                     <li key={currentPage} className={`page-item number-item`}>
-                        <Link to={`/users/${currentPage}`} className="page-link">{currentPage}</Link>
+                        <Link
+                            to={currentPage > length ? `/users/${length}` : `/users/${currentPage}`}
+                            className="page-link">
+                            {currentPage}
+                        </Link>
                     </li>
                     <li className={`page-item next-item} ${currentPage === length ? 'disabled' : ''}`}>
-                        <Link to={`/users/${currentPage + 1}`} className="page-link">&gt;&gt;</Link>
+                        <Link
+                            to={`/users/${currentPage + 1}`}
+                            className="page-link">
+                            &gt;&gt;
+                        </Link>
                     </li>
                     <li className={`page-item last-item ${currentPage === length ? 'disabled' : ''}`}>
-                        <Link to={`/users/${length}`} className="page-link">Last</Link>
+                        <Link
+                            to={`/users/${length}`}
+                            className="page-link">
+                            Last
+                        </Link>
                     </li>
                 </ul>
             }
