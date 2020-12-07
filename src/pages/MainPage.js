a20ca01fd4e153b1a4d9b2d599d71fe3608ac910
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-07 13:56:13 +0300

    pagination

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -63,15 +63,13 @@ export const MainPage = () => {
             <div className="row">
                 <div className='col s6 offset-s3'>
                     <Pagination pager={pager} />
-                </div>
-            </div>
-            <div className='row'>
-                <div className="input-field">
-                    <select value={limit} onChange={selectHandler} style={{ 'display': 'block' }}>
-                        <option value="3">Show 3 per page</option>
-                        <option value="5">Show 5 per page</option>
-                        <option value="10">Show 10 per page</option>
-                    </select>
+                    <div className="input-field">
+                        <select value={limit} onChange={selectHandler} className='select-limit'>
+                            <option value="3">Show 3 per page</option>
+                            <option value="5">Show 5 per page</option>
+                            <option value="10">Show 10 per page</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
