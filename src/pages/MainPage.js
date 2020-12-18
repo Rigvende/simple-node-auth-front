1a498f34c824094f27dfe755c4b1a9bf134451c2
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-18 12:31:50 +0300

    print download

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,18 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import { useHttp } from '../utils/http.hook';
 import { useMessage } from '../utils/message.hook';
 import { Loader } from '../components/Loader';
 import { UsersList } from '../components/UsersList';
 import { Pagination } from '../components/Pagination';
 import { useParams } from 'react-router-dom';
+// import {htmlToText} from 'html-to-text';
+import dateformat from 'dateformat';
+import printJS from 'print-js';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 export const MainPage = () => {
+    const componentRef = useRef();
     const message = useMessage();
     const { loading, error, clearError, request } = useHttp();
     const [users, setUsers] = useState([]);
@@ -31,7 +37,7 @@ export const MainPage = () => {
                 setPager({
                     currentPage: page,
                     length: Math.ceil(data.data.length / data.data.limit),
-                    limit    
+                    limit
                 });
             }
         } catch (err) { }
@@ -53,18 +59,49 @@ export const MainPage = () => {
         }
     }
 
+    const handlePrint = () => {
+        printJS('users-save', 'html');
+    }
+
+    const handleDownload = () => {
+        html2canvas(document.querySelector("#users-save")).then(canvas => {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF();
+            pdf.addImage(imgData, 'PNG', 0, 0);
+            const date = dateformat(new Date(), 'yyyymmdd');
+            const fileName = `Users-${date}.txt`;
+            pdf.save(fileName);
+        });
+    };
+
     return (
-        <div>
+        <div id='users-save' ref={componentRef} >
             <div className='row'>
-                <div className='col s6 offset-s3'>
+                <div className='col s6 offset-s3' >
                     <h2>Users</h2>
                     {users ? !loading && <UsersList users={users} /> : null}
                 </div>
             </div>
+
             <div className="row">
                 <div className='col s6 offset-s3'>
                     <Pagination pager={pager} handler={selectHandler} />
                 </div>
+            </div>
+
+            <div className="card-action center">
+                <button
+                    className='btn yellow darken-4'
+                    disabled={loading}
+                    onClick={handlePrint}>
+                    Print
+                </button>
+                <button
+                    className='btn grey'
+                    disabled={loading}
+                    onClick={handleDownload}>
+                    Download
+                </button>
             </div>
         </div>
     )
