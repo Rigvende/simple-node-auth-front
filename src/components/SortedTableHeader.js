ca41687d48f94b85450e4bb1cfd637207b165a9f
Marianna Patrusova <Marianna.Patrusova@itechart-group.com>
2020-12-21 16:44:48 +0300

    sorted table header

---
+++ src/components/SortedTableHeader.js
@@ -0,0 +1,66 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const descendingComparator =
+    (a, b, orderBy) => (b[orderBy] < a[orderBy]) ? -1 : (b[orderBy] > a[orderBy]) ? 1 : 0;
+
+export const getComparator = (order, orderBy) =>
+    order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+
+export const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        return (order !== 0) ? order : a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+};
+
+export default function SortedTableHeader(props) {
+    const { order, orderBy, onRequestSort, columns, operations } = props;
+    const createSortHandler = property => event => onRequestSort(event, property);
+
+    return (
+        <thead>
+            <tr>
+                {columns.map(column => {
+                    let label = column.label;
+                    return (
+                        <th className='pointer'
+                            key={column.id}
+                            sortDirection={orderBy === column.id ? order : false}>
+                            <div
+                                direction={orderBy === column.id ? order : 'asc'}
+                                onClick={createSortHandler(column.id)}>
+                                {label}
+                                {orderBy === column.id
+                                    ? order === 'desc'
+                                        ? <i class="tiny material-icons">arrow_drop_down</i>
+                                        : <i class="tiny material-icons">arrow_drop_up</i>
+                                    : null}
+                            </div>
+                        </th>
+                    )
+                })}
+                {operations
+                    ? <th
+                        key='operations'
+                        align='center'
+                        className='pointer'>
+                        Operations
+                    </th>
+                    : null}
+            </tr>
+        </thead>
+    );
+};
+
+SortedTableHeader.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    columns: PropTypes.array.isRequired,
+    operations: PropTypes.bool,
+};
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,13 +1,27 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { useHttp } from '../utils/http.hook';
 import { useMessage } from '../utils/message.hook';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import SortedTableHeader, { getComparator, stableSort } from "./SortedTableHeader";
+
+const columns = [
+    { id: "name", label: "Name" },
+    { id: "age", label: "Age" },
+];
 
 export const UsersList = ({ users }) => {
     const { request, error, clearError } = useHttp();
     const message = useMessage();
     const { idUser } = useContext(AuthContext);
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('name');
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
 
     useEffect(() => {
         message(error);
@@ -24,38 +38,42 @@ export const UsersList = ({ users }) => {
 
     return (
         <table className="highlight centered">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Operations</th>
-                </tr>
-            </thead>
+            <SortedTableHeader
+                columns={columns}
+                order={order}
+                orderBy={orderBy}
+                operations={true}
+                onRequestSort={handleRequestSort}
+            />
 
             <tbody>
-                {users.map((user, index) => {
-                    return (
-                        <tr id={user.id} key={index}>
-                            <td className={idUser === user.id ? `user` : null}>{user.name}</td>
-                            <td>{user.age}</td>
-                            <td>
-                                <Link
-                                    className="button-link button-blue"
-                                    to={`/edit/${user.id}`}>
-                                    Edit
-                                </Link>
-                                <button
-                                    className="button-link button-orange"
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                        deleteHandler(user.id);
-                                    }}>
-                                    Delete
-                                </button>
-                            </td>
-                        </tr>
-                    )
-                })}
+                {stableSort(users, getComparator(order, orderBy))
+                    .map((user, index) => {
+                        return (
+                            <tr id={user.id} key={index}>
+                                <td className={idUser === user.id ? `user` : null}>{user.name}</td>
+
+                                <td>{user.age}</td>
+
+                                <td>
+                                    <Link
+                                        className="button-link button-blue"
+                                        to={`/edit/${user.id}`}>
+                                        Edit
+                                    </Link>
+
+                                    <button
+                                        className="button-link button-orange"
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            deleteHandler(user.id);
+                                        }}>
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    })}
             </tbody>
         </table>
     )
